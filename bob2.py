0691a38f363bec85ba1751ac4c9d42375305a004
DevTuple <41530582+tuple0110@users.noreply.github.com>
2023-08-25 11:48:55 +0900

    Update bob2.py

--- bob2.py
+++ bob2.py
@@ -1,12 +1,26 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from datetime import date
+import re
 
-html = urlopen("http://jeju-s.jje.hs.kr/jeju-s")
+today = date.today().strftime("%Y/%m/%d")
+
+html = urlopen(f"https://jeju-s.jje.hs.kr/jeju-s/food/{today}/breakfast")
 soup = BeautifulSoup(html, "html.parser")
-bap = soup.select(".meal_menu ul li")
+breakfast = re.sub("<[^<>]*>", "", str(soup.select(".ulType_food > li:nth-child(2) > dl > dd")[0]).replace("<br/>", "\n"))
 
-print("=" * 50)
-menuList = bap[-2].text.split()
-for i in range(len(menuList)):
-    print(f"{i + 1}: {menuList[i]}")
-print("=" * 50)
+html = urlopen(f"https://jeju-s.jje.hs.kr/jeju-s/food/{today}/lunch")
+soup = BeautifulSoup(html, "html.parser")
+lunch = re.sub("<[^<>]*>", "", str(soup.select(".ulType_food > li:nth-child(2) > dl > dd")[0]).replace("<br/>", "\n"))
+
+html = urlopen(f"https://jeju-s.jje.hs.kr/jeju-s/food/{today}/dinner")
+soup = BeautifulSoup(html, "html.parser")
+dinner = re.sub("<[^<>]*>", "", str(soup.select(".ulType_food > li:nth-child(2) > dl > dd")[0]).replace("<br/>", "\n"))
+
+print("===================================================")
+print(breakfast)
+print("===================================================")
+print(lunch)
+print("===================================================")
+print(dinner)
+print("===================================================")
